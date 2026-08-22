ab2d5856fb4ef6be2733ac5429649147bbcef2b8
sajadanjidani <sajadanjiidanii@gmail.com>
2026-08-22 18:31:35 +0330

    feat: complete ListItem component

--- src/layout/Navbar/Navbar.jsx
+++ src/layout/Navbar/Navbar.jsx
@@ -1,20 +1,22 @@
-import { Link } from "react-router-dom";
-import { useState } from "react";
+import { Link, useLocation } from "react-router-dom";
+import { useEffect, useState } from "react";
 
 // import listItem
 import ListItem from "./elements/ListItem";
 import IconButton from "./elements/IconButton";
 
 export default function Navbar() {
-
+  
   const [listItemInfo , setListItemInfo] = useState([
-    {id : 1 , title : 'Home' , hrefLink : '/' },
-    {id : 2 , title : 'Pricing' , hrefLink : '/' },
-    {id : 3 , title : 'Movies' , hrefLink : '/' },
-    {id : 4 , title : 'Series' , hrefLink : '/' },
-    {id : 5 , title : 'Collection' , hrefLink : '/' },
-    {id : 6 , title : 'FAQ' , hrefLink : '/' },
+    {id : 1 , title : 'Home' , hrefLink : '/'},
+    {id : 2 , title : 'Pricing' , hrefLink : '/Pricing'},
+    {id : 3 , title : 'Movies' , hrefLink : '/Movies'},
+    {id : 4 , title : 'Series' , hrefLink : '/Series'},
+    {id : 5 , title : 'Collection' , hrefLink : '/Collection'},
+    {id : 6 , title : 'FAQ' , hrefLink : '/FAQ'},
   ])
+  
+  const { pathname } = useLocation()
 
   return (
     <div className='flex items-center justify-between static mt-4 rounded-2xl w-full h-20 px-12.5 container bg-white/20 **:text-[#091E51] **:hover:text-[#228EE5]'>
@@ -25,7 +27,7 @@ export default function Navbar() {
       {/* menu */}
         <ul className="flex items-center gap-7 h-full">
           {listItemInfo.map((item) => 
-            <ListItem key={item.id} {...item}/>
+            <ListItem key={item.id} {...item} isActive={pathname === item.hrefLink}/>
           )}
         </ul>
       {/* icon button */}
--- src/layout/Navbar/elements/listItem.jsx
+++ src/layout/Navbar/elements/listItem.jsx
@@ -1,9 +1,10 @@
-import { Link } from "react-router-dom"
+import { NavLink } from "react-router-dom"
 
-export default function ListItem({hrefLink , title}) {
+export default function ListItem({hrefLink , title , isActive}) {
   return (
-    <Link to={hrefLink}>
+    <NavLink to={hrefLink}>
         <li>{title}</li>
-    </Link>
+        {isActive && <div className="mx-auto -mb-2 w-full h-1 bg-[#228EE5] blur-xs rounded-full"></div>}
+    </NavLink>
   )
 }
