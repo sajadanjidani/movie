6c5ecbae8a3805272b974c01ecf24d1b51fb44cb
sajadanjidani <sajadanjiidanii@gmail.com>
2026-08-23 21:58:46 +0330

    feat: add Home page

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
+// layout
 import Navbar from "./layout/Navbar/Navbar"
+// page
+import Home from "./pages/Home/Home"
 
 function App() {
 
   return (
     <>
       <Navbar/>
+      <Home/>
     </>
   )
 }
