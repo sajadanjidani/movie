dce02fbeb0e94e5dcddf8ec6d50e11b1aa159ede
sajadanjidani <sajadanjiidanii@gmail.com>
2026-08-26 14:36:08 +0330

    feat: complete Card and CardButton components

--- src/components/Card/CardButton.jsx
+++ src/components/Card/CardButton.jsx
@@ -1,14 +1,28 @@
-// icons
 import { FaPlus } from "react-icons/fa";
 
 export default function CardButton() {
+
+  const clickHandler = (event) => {
+    event.preventDefault();
+    event.stopPropagation();
+
+    // کاری که می‌خواهی با کلیک روی دکمه انجام شود
+    console.log("Card Button Click");
+  };
+
   return (
-    <div className="absolute w-15 h-15 border-b-8 border-r-8 border-[#EBFAFF] dark:border-[#030A1B] rounded-br-lg rounded-tl-lg">
-        <div className="absolute -top-1.5 -left-1  w-16 h-16 border-8 border-[#EBFAFF] dark:border-[#030A1B] rounded-2xl flex justify-center items-center">
-            <button className="hover:cursor-pointer w-full h-full flex justify-center items-center">
-                <FaPlus className="text-white size-6"/>
-            </button>
-        </div>
+    <div className="absolute z-50 w-15 h-15 border-b-8 border-r-8 border-[#EBFAFF] dark:border-[#030A1B] rounded-br-lg rounded-tl-lg">
+      <div className="absolute -top-1.5 -left-1 w-16 h-16 border-8 border-[#EBFAFF] dark:border-[#030A1B] rounded-2xl flex justify-center items-center">
+        
+        <button
+          type="button"
+          onClick={clickHandler}
+          className="hover:cursor-pointer w-full h-full flex justify-center items-center"
+        >
+          <FaPlus className="text-white size-6" />
+        </button>
+
+      </div>
     </div>
-  )
+  );
 }
