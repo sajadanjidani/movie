4dd9ed9138e63a231bc01a423290266d34afa9bc
sajadanjidani <sajadanjiidanii@gmail.com>
2026-09-09 15:08:20 +0330

    feat: add ActorsSlider component

--- src/components/ActorsSection/ActorsSection.jsx
+++ src/components/ActorsSection/ActorsSection.jsx
@@ -1,9 +1,39 @@
-import React from 'react'
+import { Link } from "react-router-dom";
+import { CategorySliderProvider } from "../../context/CategorySliderContext";
 
-export default function ActorsSection() {
+import ActorsSlider from './components/ActorsSlider/ActorsSlider'
+
+// icons
+import { FaArrowRight } from "react-icons/fa";
+
+export default function ActorsSection({moreAddress}) {
   return (
-    <div>
-      
-    </div>
+    <CategorySliderProvider>
+
+      <div className="w-full h-auto mt-24 px-14">
+
+        <div className="flex items-center justify-between">
+
+          <h2 className="font-bold text-4xl">
+            Actors
+          </h2>
+
+          {moreAddress && (
+            <Link 
+              to={moreAddress}
+              className="flex items-center gap-2 font-semibold text-[#228EE5]"
+            >
+              See More
+              <FaArrowRight />
+            </Link>
+          )}
+
+        </div>
+
+          <ActorsSlider />
+
+      </div>
+
+    </CategorySliderProvider>
   )
 }
---
+++ src/components/ActorsSection/components/ActorsSlider/ActorsSlider.jsx
@@ -0,0 +1,86 @@
+import useEmblaCarousel from "embla-carousel-react";
+import { useCategorySlider } from "../../context/CategorySliderContext";
+
+
+export default function ActorsSlider({
+  items = []
+  
+  }) {
+
+  const {
+    selectedCategories = []
+  } = useCategorySlider();
+
+
+
+  let filteredData = items;
+
+
+
+  if (selectedCategories.length > 0) {
+
+    filteredData = items.filter(item =>
+
+      item.genres?.some(genre =>
+
+        selectedCategories.some(selected =>
+
+          genre.toLowerCase().trim() ===
+          selected.toLowerCase().trim()
+
+        )
+
+      )
+
+    );
+
+  }
+
+
+  filteredData =
+    filteredData
+      .sort(
+        (a,b) =>
+          b.rating - a.rating
+      )
+      .slice(0,10);
+
+
+
+  const [emblaRef] =
+    useEmblaCarousel({
+      loop:false,
+      dragFree:false,
+    });
+
+
+
+  return (
+
+    <div
+      ref={emblaRef}
+      className="overflow-hidden"
+    >
+
+      <div className="flex mt-5 gap-10">
+
+        {
+          filteredData.map(item => (
+
+            <ActorCard
+              key={item.id}
+              {...item}
+            />
+
+          ))
+        }
+
+
+      </div>
+
+
+    </div>
+
+  );
+
+}
