e1df6b5e32a33f3d852c9eb00588373aa468e319
sajadanjidani <sajadanjiidanii@gmail.com>
2026-09-11 17:36:56 +0330

    feat : add StudiosSection component

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -28,6 +28,9 @@ export default function Home() {
       {/* FAQ */}
       <FrequentlyQuestionsSection />
 
+      {/* Studios */}
+      
+
     </div>
   )
 }
---
+++ src/pages/Home/components/StudiosSection/StudiosSection.jsx
@@ -0,0 +1,7 @@
+export default function StudiosSection() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
