604918c32c91206547ddb03a096986df93d8faec
sajadanjidani <sajadanjiidanii@gmail.com>
2026-09-11 20:01:42 +0330

    feat: complete StudiosSection and StudiosItem components

--- src/pages/Home/components/StudiosSection/StudiosSection.jsx
+++ src/pages/Home/components/StudiosSection/StudiosSection.jsx
@@ -1,11 +1,25 @@
 import StudiosItem from "./element/StudiosItem"
+import { StudiosDatas } from "../../data/StudiosDatas"
+
+import { useTheme } from '@/context/ThemeContext'
 
 export default function StudiosSection() {
+
+    const datas = StudiosDatas
+    const { darkMode } = useTheme();
+
+
   return (
     <div>
-      <h2 className="text-4xl font-bold text-center">Studios</h2>
-      <div className="w-4/5 mx-auto my-15 h-100 grid grid-cols-5 grid-rows-2 gap-16">
-        <StudiosItem />
+      <h2 className="text-5xl font-bold text-center">Studios</h2>
+      <div className="w-4/5 mx-auto my-15 h-100 grid justify-items-center items-center grid-cols-5 grid-rows-2 gap-4">
+        {datas.map(data => (
+            darkMode ? (
+                <StudiosItem key={data.id} alt={data.alt} srcImage={data.dark}/>
+            ) : (
+                <StudiosItem key={data.id} alt={data.alt} srcImage={data.light}/>
+            )
+        ))}
       </div>
     </div>
   )
--- src/pages/Home/components/StudiosSection/element/StudiosItem.jsx
+++ src/pages/Home/components/StudiosSection/element/StudiosItem.jsx
@@ -1,7 +1,8 @@
-export default function StudiosItem() {
+
+export default function StudiosItem({alt , srcImage}) {
   return (
-    <div className="bg-[#030A1B] dark:bg-[#EBFAFF] w-40 h-40 rounded-4xl">
-      
+    <div className="flex justify-center items-center bg-[#030A1B] dark:bg-[#EBFAFF] w-40 h-40 rounded-4xl">
+      <img alt={alt} src={srcImage} />
     </div>
   )
 }
