213158803ec56e0b8af00786f1d56bbe80582ea1
sajadanjidani <sajadanjiidanii@gmail.com>
2026-09-11 18:22:06 +0330

    feat : add StudioDatas

---
+++ src/pages/Home/data/StudiosDatas.js
@@ -0,0 +1,43 @@
+// HBO
+import HBOLight from '@/assets/images/Home/Studios/light/HBO.svg'
+import HBODark from '@/assets/images/Home/Studios/dark/HBO.svg'
+// WB
+import WBLight from '@/assets/images/Home/Studios/light/WB.svg'
+import WBDark from '@/assets/images/Home/Studios/dark/WB.svg'
+// Disney
+import DisneyLight from '@/assets/images/Home/Studios/light/Disney.svg'
+import DisneyDark from '@/assets/images/Home/Studios/dark/Disney.svg'
+// MARVEL
+import MARVELLight from '@/assets/images/Home/Studios/light/MARVEL.svg'
+import MARVELDark from '@/assets/images/Home/Studios/dark/MARVEL.svg'
+// DC
+import DCLight from '@/assets/images/Home/Studios/light/DC.svg'
+import DCDark from '@/assets/images/Home/Studios/dark/DC.svg'
+// AMC
+import AMCLight from '@/assets/images/Home/Studios/light/AMC.svg'
+import AMCDark from '@/assets/images/Home/Studios/dark/AMC.svg'
+// NETFLIX
+import NETFLIXLight from '@/assets/images/Home/Studios/light/NETFLIX.svg'
+import NETFLIXDark from '@/assets/images/Home/Studios/dark/NETFLIX.svg'
+// Paramount
+import ParamountLight from '@/assets/images/Home/Studios/light/Paramount.svg'
+import ParamountDark from '@/assets/images/Home/Studios/dark/Paramount.svg'
+// SONY
+import SONYLight from '@/assets/images/Home/Studios/light/SONY.svg'
+import SONYDark from '@/assets/images/Home/Studios/dark/SONY.svg'
+// TV
+import TVLight from '@/assets/images/Home/Studios/light/TV.svg'
+import TVDark from '@/assets/images/Home/Studios/dark/TV.svg'
+
+export const StudiosDatas = [
+    {id : 1 , alt : 'HBO', light : HBOLight, dark : HBODark},
+    {id : 2 , alt : 'WB', light : WBLight, dark : WBDark},
+    {id : 3 , alt : 'Disney', light : DisneyLight, dark : DisneyDark},
+    {id : 4 , alt : 'MARVEL', light : MARVELLight, dark : MARVELDark},
+    {id : 5 , alt : 'DC', light : DCLight, dark : DCDark},
+    {id : 6 , alt : 'AMC', light : AMCLight, dark : AMCDark},
+    {id : 7 , alt : 'NETFLIX', light : NETFLIXLight, dark : NETFLIXDark},
+    {id : 8 , alt : 'Paramount', light : ParamountLight, dark : ParamountDark},
+    {id : 9 , alt : 'SONY', light : SONYLight, dark : SONYDark},
+    {id : 10 , alt : 'TV', light : TVLight, dark : TVDark},
+]
