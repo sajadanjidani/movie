271a25902a098eca9d39616bbf69db09be2b65c8
sajadanjidani <sajadanjiidanii@gmail.com>
2026-09-11 18:05:11 +0330

    feat: add studio section images

--- src/pages/Home/components/StudiosSection/StudiosSection.jsx
+++ src/pages/Home/components/StudiosSection/StudiosSection.jsx
@@ -1,9 +1,11 @@
+import StudiosItem from "./element/StudiosItem"
+
 export default function StudiosSection() {
   return (
     <div>
       <h2 className="text-4xl font-bold text-center">Studios</h2>
       <div className="w-4/5 mx-auto my-15 h-100 grid grid-cols-5 grid-rows-2 gap-16">
-        
+        <StudiosItem />
       </div>
     </div>
   )
---
+++ src/pages/Home/components/StudiosSection/element/StudiosItem.jsx
@@ -0,0 +1,7 @@
+export default function StudiosItem() {
+  return (
+    <div className="bg-[#030A1B] dark:bg-[#EBFAFF] w-40 h-40 rounded-4xl">
+      
+    </div>
+  )
+}
